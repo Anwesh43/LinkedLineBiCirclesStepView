a375e8fda62ccd8e863e378245325ccbe0fe30f7
Anwesh43 <anweshthecool0@gmail.com>
2018-12-10 10:22:34 +0530

    adding LineBiCirclesStepView class

--- linebicirclesstepview/src/main/java/com/anwesh/uiprojects/linebicirclesstepview/LineBiCirclesStepView.kt
+++ linebicirclesstepview/src/main/java/com/anwesh/uiprojects/linebicirclesstepview/LineBiCirclesStepView.kt
@@ -74,3 +74,21 @@ fun Canvas.drawLBCSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class LineBiCirclesStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
