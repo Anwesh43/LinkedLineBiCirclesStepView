481f8334c1f2f0def9e41fabe77171452c6e3f6e
Anwesh43 <anweshthecool0@gmail.com>
2018-12-10 09:37:31 +0530

    adding libraries and defining modules

--- linebicirclesstepview/src/main/java/com/anwesh/uiprojects/linebicirclesstepview/LineBiCirclesStepView.kt
+++ linebicirclesstepview/src/main/java/com/anwesh/uiprojects/linebicirclesstepview/LineBiCirclesStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.linebicirclesstepview
 /**
  * Created by anweshmishra on 10/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val circles : Int = 6
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.5f
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
